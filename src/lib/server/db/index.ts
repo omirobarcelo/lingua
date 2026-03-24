7119b91bf48e959fb98f1c195b63c2c863c0390a
omirobarcelo <43706247+omirobarcelo@users.noreply.github.com>
2026-03-24 22:22:24 +0100

    Updated db setup to use neon when not in development

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,19 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 import * as schema from './schema';
+import { dev } from '$app/environment';
 
-const client = postgres(DATABASE_URL);
-export const db = drizzle(client, { schema });
+async function createDb() {
+	if (dev) {
+		const { drizzle } = await import('drizzle-orm/postgres-js');
+		const postgres = (await import('postgres')).default;
+		const client = postgres(DATABASE_URL);
+		return drizzle(client, { schema });
+	} else {
+		const { drizzle } = await import('drizzle-orm/neon-http');
+		const { neon } = await import('@neondatabase/serverless');
+		const client = neon(DATABASE_URL);
+		return drizzle({ client, schema });
+	}
+}
+
+export const db = await createDb();
